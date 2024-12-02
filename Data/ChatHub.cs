82fb371df8afd0932d4d13fbc948159f594cada7
M-Aitisam <aitisamahmed24@gmail.com>
2024-12-02 23:57:01 +0500

    Add project files.

---
+++ Data/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Real_Time_Chat_Application_SignalIR.Data
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            // Broadcast the message to all connected clients
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
---
+++ Data/ChatService.cs
@@ -0,0 +1,19 @@
+﻿    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+namespace Real_Time_Chat_Application_SignalIR.Data
+{
+    public class ChatService
+    {
+        public List<(string User, string Message)> Messages { get; } = new();
+
+        public event Action? OnMessagesUpdated;
+
+        public void AddMessage(string user, string message)
+        {
+            Messages.Add((user, message));
+            OnMessagesUpdated?.Invoke();
+        }
+    }
+
+}
